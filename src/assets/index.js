8d04397f078dade1b219a67987433ef598a01b5e
TheThinker277 <f20191482@hyderabad.bits-pilani.ac.in>
2026-04-26 10:27:00 +0530

    Initial portfolio commit

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import jenkins from "./tech/jenkins.png";
+import nextjs from "./tech/nextjs.png";
+import openai from "./tech/openai.png";
+import snowflake from "./tech/snowflake.png";
+import ror from "./tech/ror.png";
+import Sammat from "./company/Sammat.png"
+import ethereum from "./tech/ethereum.png"
+
+import coupa from "./company/coupa.png"
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import fossee from "./company/fossee.png";
+import xperi from "./company/xperi.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  coupa,
+  xperi,
+  fossee,
+  jenkins,
+  nextjs,
+  openai,
+  snowflake,
+  ror,
+  Sammat,
+  ethereum
+};
